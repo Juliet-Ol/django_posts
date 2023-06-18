26460539634abe8680e430ce17cc55ab418f56a3
Juliet <agutu@gmail.com>
2023-06-18 14:37:13 +0300

    create urls for our class views

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    # path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
 if settings.DEBUG:
