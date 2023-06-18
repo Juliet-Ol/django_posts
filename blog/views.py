5d4c9c371a563ffb2f56852a9b1bbaa92a608ddf
Juliet <agutu@gmail.com>
2023-06-18 15:12:37 +0300

    create update post class view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
+from django.forms.models import BaseModelForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView
+)
 from .models import Post
 
 
@@ -37,10 +45,27 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post   
     fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post   
+    fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)  
+
+    def test_func(self):
+        post = self.get_object  
+        if self.request.user == post.author:
+            return True
+        return False
+            
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
